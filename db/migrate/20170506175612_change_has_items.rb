714e78e00d781ba28c17393f93958ff893d663da
Ahmad Al Msouty <ahmad.msouti@Hotmail.com>
2017-05-06 21:12:36 +0300

    fixing

--- db/migrate/20170506175612_change_has_items.rb
+++ db/migrate/20170506175612_change_has_items.rb
@@ -1,6 +1,6 @@
 class ChangeHasItems < ActiveRecord::Migration[5.0]
   def change
-      remove_column :has_items, :inOrder
+      remove_column :items, :inOrder
       add_column :has_items, :quantity, :integer
       add_column :has_items, :color, :string
   end
