c72ea03efbcca3d831a297b3ece4b2443cfca29b
Ahmad Al Msouty <ahmad.msouti@Hotmail.com>
2017-05-07 06:13:36 +0300

    remove not importamt data

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,27 +3,27 @@ class PagesController < ApplicationController
 
   def home
   end
-  def check
-    item = Item.find(params[:item_id])
-    user = User.find(params[:user_id])
-    color = params[:color]
-    quantity = params[:quantity]
-    if quantity.to_i > item.itemstockqty
-      render json: { failed: "There are only #{item.itemstockqty} left in stock"}, status: :failed
-    else
-      user.orders.first.items << item
-      price = user.orders.first.orderInvoice + item.itemprice
-      user.orders.first.update(orderInvoice: price)
-      ss = HasItem.where(item_id: item.id, order_id: user.orders.first.id).update(color: color, quantity: quantity)
-      # raise 'eh'
-      # raise 'he'
-      # order = user.orders.first
-      # order.update(orderInvoice: order.orderInvoice+item.itemprice)
-      # s = HasItem.where(item_id: params[:item_id], order_id: user.orders.first.id).first
-      #   s.update(quantity: params[:quantity], color: params[:color])
-        render json: { created: true}, status: :created
-    end
+  # def check
+  #   item = Item.find(params[:item_id])
+  #   user = User.find(params[:user_id])
+  #   color = params[:color]
+  #   quantity = params[:quantity]
+  #   if quantity.to_i > item.itemstockqty
+  #     render json: { failed: "There are only #{item.itemstockqty} left in stock"}, status: :failed
+  #   else
+  #     user.orders.first.items << item
+  #     price = user.orders.first.orderInvoice + item.itemprice
+  #     user.orders.first.update(orderInvoice: price)
+  #     ss = HasItem.where(item_id: item.id, order_id: user.orders.first.id).update(color: color, quantity: quantity)
+  #     # raise 'eh'
+  #     # raise 'he'
+  #     # order = user.orders.first
+  #     # order.update(orderInvoice: order.orderInvoice+item.itemprice)
+  #     # s = HasItem.where(item_id: params[:item_id], order_id: user.orders.first.id).first
+  #     #   s.update(quantity: params[:quantity], color: params[:color])
+  #       render json: { created: true}, status: :created
+  #   end
 
 
-  end
+  # end
 end
